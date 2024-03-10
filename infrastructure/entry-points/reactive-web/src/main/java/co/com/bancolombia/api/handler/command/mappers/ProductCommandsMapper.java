9c7b8dc5a13d5a4576cf0a4d12140181ef3a112e
Daniel Ramos <dramosg@ucentral.edu.co>
2024-03-10 18:45:46 -0500

    Se completa flujo gut post put

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/handler/command/mappers/ProductCommandsMapper.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/handler/command/mappers/ProductCommandsMapper.java
@@ -1,6 +1,6 @@
-package co.com.bancolombia.api.mappers;
+package co.com.bancolombia.api.handler.command.mappers;
 
-import co.com.bancolombia.api.dto.ProductDTO;
+import co.com.bancolombia.api.handler.command.commands.ProductCommand;
 import co.com.bancolombia.model.productmodel.model.ProductModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -8,7 +8,7 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
-public interface ProductDtoMapper {
-    ProductModel toModel(ProductDTO producto);
-    ProductDTO toDTO(ProductModel producto);
+public interface ProductCommandsMapper {
+    ProductModel toModel(ProductCommand producto);
+    ProductCommand toDTO(ProductModel producto);
 }
